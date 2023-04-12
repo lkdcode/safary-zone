5d2861650bd871efe81a886dcf856e56ae867b2e
JeFeel <jaephil94@gmail.com>
2023-04-12 14:12:08 +0900

    test commit

--- src/main/java/controller/menu/capture/view/InputView.java
+++ src/main/java/controller/menu/capture/view/InputView.java
@@ -4,4 +4,5 @@ package controller.menu.capture.view;
  * 유저로부터 입력을 받는 역할
  */
 public class InputView {
+
 }
--- src/main/java/controller/menu/capture/view/OutputView.java
+++ src/main/java/controller/menu/capture/view/OutputView.java
@@ -4,4 +4,6 @@ package controller.menu.capture.view;
  * 콘솔에 출력하는 역할
  */
 public class OutputView {
+
+    private final String ENCOUNTER;
 }
