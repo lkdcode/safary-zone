e5182788c954321822aa4e04a265f80ceea69967
lkdcode <dlrlejr1@naver.com>
2023-04-13 02:39:47 +0900

    콘솔에 출력하는 역할

--- src/main/java/controller/menu/battle/view/OutputView.java
+++ src/main/java/controller/menu/battle/view/OutputView.java
@@ -12,7 +12,7 @@ public class OutputView {
     public OutputView() {
     }
 
-    public void menu(){
+    public void menu() {
         System.out.println("잠시 후 포켓몬과의 전투가 시작됩니다.");
         System.out.println("### 포켓몬 등장 ###");
         System.out.println("야생의 파이리가 등장하였습니다.");
