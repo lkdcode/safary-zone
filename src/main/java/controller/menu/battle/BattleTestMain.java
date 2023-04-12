99198a79811134ecaaefa9b53ebd6eb800538edd
lkdcode <dlrlejr1@naver.com>
2023-04-13 02:38:15 +0900

    Battle 컨텐츠 run test class

--- src/main/java/controller/menu/battle/BattleTestMain.java
+++ src/main/java/controller/menu/battle/BattleTestMain.java
@@ -1,11 +1,13 @@
 package controller.menu.battle;
 
+import controller.menu.battle.controller.BattleController;
 import controller.menu.battle.service.SummonServiceLogic;
 import pokemon.pokemon.Pokemon;
 
 public class BattleTestMain {
     public static void main(String[] args) {
 
+        new BattleController().start();
 
     }
 }
