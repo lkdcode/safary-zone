e43d10de6ffecdcd23d1dc2c33ca0965dbe70f95
JOY0987 <jjoyewon@gmail.com>
2023-04-12 14:13:30 +0900

    정원 출력

---
+++ src/main/java/controller/menu/garden/TestMain.java
@@ -0,0 +1,10 @@
+package controller.menu.garden;
+
+import controller.menu.garden.controller.GardenController;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        new GardenController().menu();
+    }
+}
--- src/main/java/controller/menu/garden/controller/GardenController.java
+++ src/main/java/controller/menu/garden/controller/GardenController.java
@@ -1,11 +1,40 @@
 package controller.menu.garden.controller;
 
+import controller.menu.garden.view.InputView;
+import controller.menu.garden.view.OutputView;
+
 /**
  * 정원 controller
  */
 public class GardenController {
 
-    public void menu() {
+    private final String CHECK_MY_GARDEN = "1";
+    private final String PLANTING_BERRY = "2";
+    private final String EXIT = "0";
 
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GardenController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void menu() {
+        while(true) {
+            outputView.showGardenMenu();
+            String menu = inputView.inputMenu();
+
+            switch (menu) {
+                case CHECK_MY_GARDEN :
+                    outputView.showMyGarden();
+                    break;
+                case PLANTING_BERRY :
+                    break;
+                case EXIT :
+                    break;
+                default:
+            }
+        }
     }
 }
