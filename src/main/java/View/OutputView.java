ee5d60a62cc4dddb90ee9de7bfa69e0fea403413
lkdcode <dlrlejr1@naver.com>
2023-04-14 19:48:27 +0900

    메인 메뉴 출력 문자 변경

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,4 +1,4 @@
-package controller;
+package View;
 
 
 import static common.MakeCommon.*;
@@ -6,26 +6,30 @@ import static common.MakeCommon.*;
 public class OutputView {
     private final int MILLIS = 30;
 
-    //메인메뉴를 보여준다
-    public void menu() {
-
-        System.out.println("1번 매뉴");
-        System.out.println("2번 매뉴");
-        System.out.println("3번 매뉴");
-        System.out.println("4번 매뉴");
-        System.out.println("5번 매뉴");
-        System.out.println("6번 매뉴");
-        System.out.println("7번 매뉴");
-
+    public void welcome() {
+        System.out.println("  🐶🐱🐭🐹🐰🦊🐻🐼🐨🐯🦁🐮🐷🐸🐵");
+        System.out.println("➖➖➖➖➖ Safary - Zone ➖➖➖➖➖");
+        System.out.println("🔻 사파리 존에 오신 여러분들을 환영합니다.");
+        System.out.println("🔻 포켓몬스터와 함께 성장하는 게임입니다.");
+        System.out.println("🔻 상점에서 씨앗을 사서 정원을 가꾸고,");
+        System.out.println("🔻 몬스터 볼로 다양한 포켓몬들을 포획해보세요.");
+        System.out.println("🔻 야생의 포켓몬스터와 대결도 할 수 있습니다.");
+        System.out.println("🔻 전설의 포켓몬도 획득할 수 있습니다!");
+        System.out.println("➖➖➖➖➖  Pokemonster  ➖➖➖➖➖");
+        System.out.println("  🐵🐸🐷🐮🦁🐯🐨🐼🐻🦊🐰🐹🐭🐱🐶\n\n\n");
     }
 
-    public void welcome() {
-        System.out.println("게임 사용 설명서 ....... 개요 등");
-        System.out.println("게임 사용 설명서 ....... 개요 등");
-        System.out.println("게임 사용 설명서 ....... 개요 등");
-        System.out.println("게임 사용 설명서 ....... 개요 등");
-        System.out.println("게임 사용 설명서 ....... 개요 등");
-        System.out.println("게임 사용 설명서 ....... 개요 등");
+    public void menu() {
+        System.out.println("➖➖➖➖➖  메인 메뉴  ➖➖➖➖➖");
+        System.out.println("🔻 1번 상점가기");
+        System.out.println("🔻 2번 정원 가꾸러 가기");
+        System.out.println("🔻 3번 포획하기");
+        System.out.println("🔻 4번 배틀라운드");
+        System.out.println("🔻 5번 내 정보 보기");
+        System.out.println("🔻 6번 저장/불러오기");
+        System.out.println("🔻 7번 종료하기\n");
+        System.out.println("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖");
+
     }
 
     public void shop() {
