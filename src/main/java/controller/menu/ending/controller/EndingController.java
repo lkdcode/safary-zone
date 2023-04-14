930276ae89d229538ce5695f908f9e1eb4dd1e15
lkdcode <dlrlejr1@naver.com>
2023-04-14 20:19:05 +0900

    엔딩 컨텐츠 구현

--- src/main/java/controller/menu/ending/controller/EndingController.java
+++ src/main/java/controller/menu/ending/controller/EndingController.java
@@ -13,14 +13,7 @@ public class EndingController {
     }
 
     public void check() {
-        // 특정 조건을 계속 체크
-        // 열매가 모두 1이상이어야 하고
-        // 플레이어의 레벨이 10이어야한다
-
-        if (endingServiceLogic.check()) {
-            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            System.out.println("엔딩 조건 달성 완료!!!!!!!!!!!!!!!!!!!");
-            System.out.println("전설의 포켓몬을 하나 얻습니다!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        }
+        if (endingServiceLogic.check()) outputView.ending();
     }
+
 }
--- src/main/java/controller/menu/ending/service/EndingServiceLogic.java
+++ src/main/java/controller/menu/ending/service/EndingServiceLogic.java
@@ -1,17 +1,36 @@
 package controller.menu.ending.service;
 
+import pokemon.books.LegendPokemonBooks;
 import user.Player;
+
+import static common.MakeCommon.getRandom;
 
 public class EndingServiceLogic {
 
+    private final int MAX_LEVEL = 10;
+    private final int NOTHING = 0;
+
 
     public boolean check() {
-        return (checkGarden() && checkGarden());
+        boolean isEnding = checkGarden() && checkLevel();
+        if (isEnding) getLegendPokemon();
+
+        return isEnding;
+    }
+
+    private void getLegendPokemon() {
+        // 플레이어에게 전설의 포켓몬스터를 추가한다.
+        int legendBookSize = LegendPokemonBooks.values().length;
+        int bookNumber = (int) getRandom(1, legendBookSize);
+
+        Player.getInstance().getPokemonList().playerPokemonList().put(
+                bookNumber, LegendPokemonBooks.values()[bookNumber]
+        );
     }
 
 
     private boolean checkLevel() {
-        return Player.getInstance().getLevel() == 10;
+        return Player.getInstance().getLevel() == MAX_LEVEL;
     }
 
     private boolean checkGarden() {
@@ -19,11 +38,12 @@ public class EndingServiceLogic {
 
         for (int i = 0; i < checkGarden.length; i++) {
             for (int j = 0; j < checkGarden[i].length; j++) {
-                if (checkGarden[i][j] == 0) return false;
+                if (checkGarden[i][j] == NOTHING) return false;
             }
         }
 
         return true;
     }
+
 
 }
