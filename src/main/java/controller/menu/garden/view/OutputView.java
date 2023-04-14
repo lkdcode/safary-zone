48b124758099b8967652626c77d237666b4f4fb4
JOY0987 <jjoyewon@gmail.com>
2023-04-14 14:12:15 +0900

    Garden, Shop 수정

--- src/main/java/controller/menu/garden/view/OutputView.java
+++ src/main/java/controller/menu/garden/view/OutputView.java
@@ -13,33 +13,27 @@ public class OutputView {
     private final String GARDEN_WELCOME_MESSAGE = "\n🌲 정원에 오신 걸 환영합니다.";
     private final String GARDEN_FIRST_MENU = "1. 🌱 나의 정원 보기";
     private final String GARDEN_SECOND_MENU = "2. 🍓 열매 심기";
-    private final String BACK_MENU = "0. 🔙 돌아가기\n";
+    private final String BACK_MENU = "0. 🔙 돌아가기";
     private final String PLANTING_BLUE_BERRY = "1. 🍇 블루베리 심기 👉 [일정 확률로 상,하로 열매를 뿌립니다.]";
     private final String PLANTING_RASP_BERRY = "2. 🍓 라즈베리 심기 👉 [일정 확률로 좌,우로 열매를 뿌립니다.]";
     private final String PLANTING_BLACK_BERRY = "3. ♣️ 블랙베리 심기 👉 [일정 확률로 상,하,좌,우로 열매를 뿌립니다.]";
-    private final String BACK = "\n돌아갑니다.\n\n";
+    private final String BACK = "\n돌아갑니다.\n";
     private final int MILLIS = 30;
     private final int SHORT_MILLS = 10;
 
     public void showGardenMenu() {
-        String message = GARDEN_WELCOME_MESSAGE
-                + System.lineSeparator()
-                + GARDEN_FIRST_MENU
-                + System.lineSeparator()
-                + GARDEN_SECOND_MENU
-                + System.lineSeparator()
-                + BACK_MENU;
+        String message = GARDEN_WELCOME_MESSAGE + System.lineSeparator()
+                + GARDEN_FIRST_MENU + System.lineSeparator()
+                + GARDEN_SECOND_MENU + System.lineSeparator()
+                + BACK_MENU + System.lineSeparator();
         dottedPrint(message, MILLIS);
     }
 
     public void showPlantingMenu() {
-        String message = PLANTING_BLUE_BERRY
-                + System.lineSeparator()
-                + PLANTING_RASP_BERRY
-                + System.lineSeparator()
-                + PLANTING_BLACK_BERRY
-                + System.lineSeparator()
-                + BACK_MENU;
+        String message = PLANTING_BLUE_BERRY + System.lineSeparator()
+                + PLANTING_RASP_BERRY + System.lineSeparator()
+                + PLANTING_BLACK_BERRY + System.lineSeparator()
+                + BACK_MENU + System.lineSeparator();
         dottedPrint(message, SHORT_MILLS);
     }
 
