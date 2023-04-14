77cbb9b263da7dbf89cf553feb3e95fa097741f3
lkdcode <dlrlejr1@naver.com>
2023-04-14 20:57:32 +0900

    initial commit

---
+++ src/main/java/controller/EndingTestMain.java
@@ -0,0 +1,14 @@
+package controller;
+
+import controller.menu.ending.view.OutputView;
+
+/**
+ * main controller
+ */
+public class EndingTestMain {
+    public static void main(String[] args) {
+
+        OutputView outputView = new OutputView();
+        outputView.ending();
+    }
+}
